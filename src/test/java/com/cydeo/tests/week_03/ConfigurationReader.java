1dd177b9bba61ad9eb64d8c4a02e9eff9bfc02f6
AlenaD-B27 <lenador.18@gmail.com>
2022-10-19 14:52:59 -0500

    Configuration Reader

---
+++ src/test/java/com/cydeo/tests/week_03/ConfigurationReader.java
@@ -0,0 +1,31 @@
+package com.cydeo.tests.week_03;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class ConfigurationReader {
+
+    private static Properties properties = new Properties();
+
+    static {
+
+        FileInputStream fileInputStream = null;
+        try {
+            fileInputStream = new FileInputStream("config.properties");
+
+            properties.load(fileInputStream);
+        } catch (Exception e) {
+            System.out.println("We have problem with in ConfigurationReader class");
+        }
+
+    }
+
+    public static String getValue(String key){
+        return properties.getProperty(key);
+    }
+
+
+
+
+
+}
